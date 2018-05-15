ba063aaeb73e64fd0ed36820e2507cc3a4e92d51
Joaquín Pérez Realini <jperezr21@users.noreply.github.com>
2018-05-15 08:16:34 -0300

    Implementar reproducir canción y cerrar sesión (#1)

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const Datastore = require('@google-cloud/datastore');
 const projectId = 'newagent-6f7b4';
 const accessTokensDatastoreKind = 'spotifyAccessTokens';
 const spotifyCredentialsDatastoreKind = 'spotifyCredentials';
-const authenticatedActions = ['PlaySong', 'Login'];
+const authenticatedActions = ['PlaySong', 'Login', 'Logout'];
+const spotifyActions = ['PlaySong']
 
 /**
  * Responds to any HTTP request that can provide a "message" field in the body.
@@ -24,13 +25,26 @@ exports.fulfillmentHandler = (req, res) => {
       console.log('accessToken @ 23: ' + accessToken);
       if (!accessToken || intent == 'Login') {
         handleLogin(res, userId);
+      } else if (intent == 'Logout') {
+        handleLogout(res, userId);
       } else {
-        switch (intent) {
-          case 'PlaySong':
-            handlePlaySong(req, res, accessToken);
-            break;
-          default:
-            sendResponse(res, 'Intent not implemented');
+        if (spotifyActions.includes(intent)) {
+          getSpotifyCredentials().then(
+            spotifyCredentials => {
+              console.log('spotifyCredentials: ' + JSON.stringify(spotifyCredentials));
+              var spotifyApi = new SpotifyWebApi({
+                clientId: spotifyCredentials.clientId,
+                clientSecret: spotifyCredentials.clientSecret
+              });
+              spotifyApi.setAccessToken(accessToken);
+              switch (intent) {
+                case 'PlaySong':
+                  handlePlaySong(req, res, spotifyApi);
+                  break;
+                default:
+                  sendResponse(res, 'Intent not implemented');
+              }
+            });
         }
       }
     });
@@ -45,17 +59,40 @@ function handleLogin(res, userId) {
     `https://newagent-6f7b4.appspot.com/login?user_id=${userId}`);
 }
 
-function handlePlaySong(req, res, accessToken) {
-  getSpotifyCredentials().then(
-    spotifyCredentials => {
-      console.log('spotifyCredentials: ' + JSON.stringify(spotifyCredentials));
-      var spotifyApi = new SpotifyWebApi({
-        clientId: spotifyCredentials.clientId,
-        clientSecret: spotifyCredentials.clientSecret
-      });
-      spotifyApi.setAccessToken(accessToken);
-      spotifyApi.play();
-      sendResponse(res, "Ok");
+function handleLogout(res, userId) {
+  deleteDatastoreItem(accessTokensDatastoreKind, userId).then(() =>
+    sendResponse(res, 'Sesión cerrada correctamente')
+  );
+}
+
+function handlePlaySong(req, res, spotifyApi) {
+  var songName = req.body.queryResult.parameters.songName;
+  var artist = req.body.queryResult.parameters.artist;
+  var query;
+  if (artist) {
+    query = `track:${songName} artist:${artist}`;
+  } else {
+    query = songName;
+  }
+  spotifyApi.searchTracks(query)
+    .then(function(data) {
+      var items = data.body.tracks.items
+      if (items.length > 0) {
+        var songUri = items[0].uri;
+        var songName = items[0].name;
+        var artist = items[0].artists[0].name;
+        console.log(`Search tracks by "${songName}" in the track name and "${artist}" in the artist name`);
+        console.log(JSON.stringify(data.body));
+        spotifyApi.play({
+          "uris": [songUri]
+        }).then(() =>
+          sendResponse(res, `Reproduciendo "${songName}" de ${artist}`)
+        );
+      } else {
+        sendResponse(res, `Lo siento, no he encontrado esa canción`)
+      }
+    }, function(err) {
+      console.log('Something went wrong!', err);
     });
 }
 
@@ -86,17 +123,27 @@ function getDatastoreItem(kind, key) {
   });
 }
 
+function deleteDatastoreItem(kind, key) {
+  var datastore = Datastore({
+    projectId: projectId
+  });
+  var key = datastore.key([kind, key]);
+  return datastore.delete(key).then(() => {
+    console.log(`Deleted user ${key}`);
+  });
+}
+
 function getUserIdFromRequestData(requestData) {
   var payload = requestData.originalDetectIntentRequest.payload;
   switch (payload.source) {
     case 'slack_testbot':
-      return payload.data.user;
+      return 'slack-' + payload.data.user;
       break;
     case 'skype':
-      return payload.data.address.user.id;
+      return 'skype-' + payload.data.address.user.id;
       break;
     case 'telegram':
-      return payload.data.message.from.id;
+      return 'telegram-' + payload.data.message.from.id;
       break;
     default:
       return 'default';
