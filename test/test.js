9851e380e2c6e9608220fdc28f07047b06d8ec6c
Joaquin Perez <jperez@dataxu.com>
2018-06-08 10:55:22 -0300

    Add unit tests

---
+++ test/test.js
@@ -0,0 +1,42 @@
+const sinon = require('sinon');
+
+const cloudFunction = require('../index');
+
+describe('Default Welcome Intent', function () {
+  it('should respond with custom welcome message', function () {
+    const req = {
+      body: {
+        queryResult: {
+          intent: {
+            displayName: "Default Welcome Intent"
+          }
+        },
+        originalDetectIntentRequest: {
+          payload: {
+            source: 'telegram',
+            data: {
+              message: {
+                chat: {
+                  first_name: 'some_name'
+                }
+              }
+            }
+          }
+        }
+      }
+    };
+    const resAPI = {
+      setHeader: function (key, value) {
+      },
+      send: function (response) {
+      }
+    };
+    const res = sinon.mock(resAPI);
+    res.expects('setHeader').once().withArgs('Content-Type', 'application/json');
+    res.expects('send').once().withArgs(JSON.stringify({fulfillmentText: 'Bienvenido/a some_name!'}));
+
+    cloudFunction.fulfillmentHandler(req, resAPI);
+
+    res.verify();
+  });
+});
